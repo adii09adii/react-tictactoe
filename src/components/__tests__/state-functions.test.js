d659ed6a6e97f91a1baff6791c974c96b706a9dd
Aditi Agarwal <aditiagarwal@aditiagarwal-macbookpro.roam.corp.google.com>
2019-12-30 10:03:41 -0800

    Addeding test cases

---
+++ src/components/__tests__/state-functions.test.js
@@ -0,0 +1,54 @@
+import { setWinner, resetState, updateStateValues, setNewHistory, defaultState } from '../state-functions.js';
+
+describe('Addition', () => {
+    it('knows that 2 and 2 make 4', () => {
+        expect(2 + 2).toBe(4);
+    });
+});
+
+describe('#setWinner', () => {
+    it('sets winner X', () => {
+        const winner = 'X';
+        expect(setWinner(winner)).toEqual({winner: 'X'});
+    })
+});
+
+describe('#resetState', () => {
+    it('resets state to default', () => {
+        const defaultObj = {hello: 'world'}
+        expect(resetState(defaultObj)).toEqual({hello: 'world'});
+    });
+});
+
+describe('#updateStateValues', () => {
+    it('sets new values when player inputs', () => {
+        const curState = { foo: 'bar'};
+        expect(updateStateValues(curState, { foo: 'baz'})).toEqual({foo: 'baz'});
+    });
+
+    it('sets new values when player inputs', () => {
+        const curState = { foo: 'bar', bar: 'bar'};
+        expect(updateStateValues(curState, { foo: 'baz'})).toEqual({foo: 'baz', bar: 'bar'});
+    });
+});
+
+describe('#setNewHistory', () => {
+    it('sets new history values as players play', () => {
+        const curHistory = [['O', 'X']];
+        const newValues = ['X', 'O'];
+        expect(setNewHistory(curHistory, newValues)).toEqual([['O','X'], ['X', 'O']]);
+    });
+});
+
+describe('#defaultState', () => {
+    const defaultValues = Array(9).fill('');
+    it('sets values to default state', () => {
+        expect(defaultState()).toEqual({
+            currentText: 'O',
+            values: defaultValues,
+            winner: null,
+            history: [],
+            gameover: false
+        });
+    });
+});
---
+++ src/components/state-functions.js
@@ -0,0 +1,31 @@
+export function setWinner(winner) {
+	return { winner };
+}
+
+export function resetState(defaultObj) {
+	return defaultObj;
+}
+
+export function updateStateValues(state, obj) {
+	const newState = Object.assign(state, obj);
+
+	return newState;
+}
+
+export function setNewHistory(history, newValues) {
+	const newHistory = [...history];
+	newHistory.push(newValues);
+	return newHistory;
+}
+
+export function defaultState() {
+	const defaultValues = Array(9).fill('');
+
+	return {
+		currentText: 'O',
+		values: defaultValues,
+		winner: null,
+		history: [],
+		gameover: false
+	};
+}
