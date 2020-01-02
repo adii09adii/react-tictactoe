6fe1bed05f7951fb8d334e7074ae45b08abe35b8
Aditi Agarwal <aditiagarwal@aditiagarwal-macbookpro.roam.corp.google.com>
2020-01-01 21:39:04 -0800

    Fix

--- src22/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Game from './Components/Game/game'
-import './index.css';
-
-// ========================================
-
-ReactDOM.render(<Game />, document.getElementById("root"));
